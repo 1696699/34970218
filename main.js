8c0c0cc6758e3b3da198d42652ad783fff8616df
1696699 <120693972+1696699@users.noreply.github.com>
2023-03-26 18:48:46 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+var canvas = document.getElementById("i"); 
+var ctx = canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 4;
+ctx.arc(100, 110, 40, 0, 2 * Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.lineWidth = 4;
+ctx.arc(200, 110, 40, 0, 2 * Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth = 4;
+ctx.arc(300, 110, 40, 0, 2 * Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle = "yellow";
+ctx.lineWidth = 4;
+ctx.arc(150, 160, 40, 0, 2 * Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle = "darkgreen";
+ctx.lineWidth = 4;
+ctx.arc(250, 160, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
